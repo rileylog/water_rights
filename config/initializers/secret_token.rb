bf3cd9b2e0bcdf1de9aea118a5096202231e0058
Logan Riley <rileylog@gmail.com>
2013-12-10 21:20:11 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaterRights::Application.config.secret_token = 'c193279ebdb91cc47be17a82daa488ccd073c424c5365802e725a85a1d4a7d7c140036fe60887a3ef545e43bb8741e8bd8eb6379df7ea54a77b426234e3be00e'
